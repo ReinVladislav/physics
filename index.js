0c4f4c08693c24000362c44a2f7f90f538ccfab2
Rein <reinwlad@mail.ru>
2024-06-16 09:51:04 +0300

    add commments

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ class RecordDto{
     }
 }
 
+function changeComment(com){
+    com.innerHTML = '';
+    com.insertAdjacentHTML('beforeend', `<p style="text-align: center; color:grean;" >Вы успешно оставили заявку. Мы свяжимся с вами в ближайшее время</p>`)
+}
+
 
  sendButton.onclick = async function() {
+    let comment = document.getElementById('name');
     let fio = document.getElementById('name').value;
     let phoneNumber = document.getElementById('phone').value;
     let connectionType = document.getElementById('gender').value;
@@ -26,7 +32,9 @@ class RecordDto{
     try {
         const resp = await fetch(url,   {method: 'POST', 
                                         headers: {'Content-Type': 'application/json;charset=utf-8'},
-                                        body: JSON.stringify(newRecordDto)}) 
+                                        body: JSON.stringify(newRecordDto)});
+                                        changeComment(comment);
+                                        
     } catch (error) {
         console.log('Ошибка отправления');
     }
